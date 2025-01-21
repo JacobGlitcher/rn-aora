4e6889cac4c0ff735afb310dfd8e29fb8bffe97e
Hakob Yeroyan <hakob.yer@gmail.com>
2025-01-21 10:22:21 +0400

    creating greeting-intro page

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,46 @@
-import {View, StyleSheet, Text} from 'react-native'
+import {View, StyleSheet, Text, ScrollView, Image} from 'react-native'
 import {StatusBar} from 'expo-status-bar'
 import {Link} from "expo-router";
+import {SafeAreaView} from "react-native-safe-area-context";
+
+import {images} from '../constants'
 
 const App = () => {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-2xl font-pbold text-red-600">Aora!</Text>
-      <StatusBar style="auto" />
-      <Link href="/(tabs)/home">Go To Home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{height: '100%'}}>
+        <View className="w-full items-center px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] h-[300px] w-full"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center px-2">
+              Discover Endless Possibilities with {''}
+              <Text className="text-secondary-200">
+                Aora
+              </Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[11px] absolute -bottom-3 -right-9"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 px-2 text-center">
+            Where creativity meets innovation: embark on a journey of limitless exploration with Aora
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
